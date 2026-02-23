bd4013557bb46ecaa5983fae672f256eeba1f9f5
Zayd <zaydbingazali@gmail.com>
2026-02-23 22:31:53 +0300

    first commit

---
+++ portal/config.js
@@ -0,0 +1,2 @@
+const SUPABASE_URL = 'https://mghouuvlnalodoxotqwo.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1naG91dXZsbmFsb2RveG90cXdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE3NTkzODYsImV4cCI6MjA4NzMzNTM4Nn0.ww6sepKzPrx46BbCAjFESpCX9B6eDJfoft08k8Xe2rw';
